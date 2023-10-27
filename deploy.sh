8d077b3965f08fdbfc15a872a75065b5066ebd38
wannisaihavecpu <145314656+wannisaihavecpu@users.noreply.github.com>
2023-10-27 15:08:15 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 sudo apt-get install -y nodejs
 node -v
+sudo apt remove nodejs
+sudo apt remove nodejs-doc
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-cd /home/ec2-user/ihavecpu_frontend
+cd /home/ubuntu/ihavecpu_frontend
 git pull origin main
 node -v
 sudo npm install -g yarn
