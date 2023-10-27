16b5a2d729f729a5624dde979322ffc7aa12d3a9
wannisaihavecpu <145314656+wannisaihavecpu@users.noreply.github.com>
2023-10-27 13:53:40 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sudo apt install curl 
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-nvm list
-nvm install node
 cd /home/ubuntu/ihavecpu_frontend
 git pull origin main
 npm install -g yarn
