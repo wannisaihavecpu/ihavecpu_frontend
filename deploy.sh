2edf7501de8389bad0f0e25fd7ac0289197acc34
wannisaihavecpu <145314656+wannisaihavecpu@users.noreply.github.com>
2023-10-27 14:18:36 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/ihavecpu_frontend
 git pull origin main
-npm install -g yarn
-yarn install &&
+sudo npm install -g yarn
+sudo yarn install &&
 yarn build &&
 pm2 restart [ihavecpu_frontend]
